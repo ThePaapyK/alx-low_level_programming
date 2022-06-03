13439bdf7fd6496005bac9b04d27fbb71648bec8
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-03 00:16:45 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,5 +11,6 @@ int main(void)
 {
 int s = strlen("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", s);
+write(1, "\n", 1);
 return (1);
 }
