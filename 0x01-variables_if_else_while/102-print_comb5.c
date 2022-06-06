82c083fb2bed004776c856e38418f70ba6c79d15
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-06 02:51:49 +0000

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,37 +9,37 @@
 
 int main(void)
 {
-	int a = '0';
-	int b = '0';
-	int c = '0';
-	int d = '0';
+int a = '0';
+int b = '0';
+int c = '0';
+int d = '0';
 
-	for (c = '0'; c <= '9'; c++)
-	{
-		for (d = '0'; d <= '9'; d++)
-		{
-			for (a = '0'; a <= '9'; a++)
-			{
-				for (b = '0'; b <= '9'; b++)
-				{
-					if (c < a || (c == a && d < b))
-					{
-						putchar(c);
-						putchar(d);
-						putchar(32);
-						putchar(a);
-						putchar(b);
+for (c = '0'; c <= '9'; c++)
+{
+for (d = '0'; d <= '9'; d++)
+{
+for (a = '0'; a <= '9'; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+if (c < a || (c == a && d < b))
+{
+putchar(c);
+putchar(d);
+putchar(32);
+putchar(a);
+putchar(b);
 
-						if ((c < '9' || d < '8') || (a < '9' || b < '9'))
-						{
-							putchar(44);
-							putchar(32);
-						}
-					}
-				}
-			}
-		}
-	}
-		putchar('\n');
-		return (0);
+if ((c < '9' || d < '8') || (a < '9' || b < '9'))
+{
+putchar(44);
+putchar(32);
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
