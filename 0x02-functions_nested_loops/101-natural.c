b950f74c8b682f1fc1c74591e88fcec28bdb7810
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-08 13:39:56 -0700

    another day, another code

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * natural - prints multiples of 3 and 5 below 1024
+ *
+ */
+
+void natural(void)
+{
+	int n;
+	int m = 0;
+
+	for (n = 1; n < 1024; n++)
+	{
+		if (((n % 3) == 0) || ((n % 5) == 0))
+		{
+			printf("%d", n);
+
+			if (n < 1023)
+			{
+				printf(",");
+				printf(" ");
+			}
+
+
+			m = m + n;
+		}
+	}
+	printf("\n");
+	printf("%d\n", m);
+}
