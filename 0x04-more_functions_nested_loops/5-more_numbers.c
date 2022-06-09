99f81dade8fd53be3ed8c6b02d034b8e3cc9453b
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-09 23:37:33 +0000

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,28 +7,28 @@
 
 void more_numbers(void)
 {
-	int count;
-	int n;
-	int k;
-	int r;
+int count;
+int n;
+int k;
+int r;
 
-	count = 1;
+count = 1;
 
-	while (count <= 10)
-	{
-		for (n = 0; n <= 14; n++)
-		{
-				k = n / 10;
-				r = n % 10;
+while (count <= 10)
+{
+for (n = 0; n <= 14; n++)
+{
+k = n / 10;
+r = n % 10;
 
 
-			if (n > 9)
-			{
-				_putchar(k + '0');
-			}
-			_putchar(r + '0');
-		}
-		_putchar('\n');
-		count++;
-	}
+if (n > 9)
+{
+_putchar(k + '0');
+}
+_putchar(r + '0');
+}
+_putchar('\n');
+count++;
+}
 }
