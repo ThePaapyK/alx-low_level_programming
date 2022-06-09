5b9058ab3d5eea6ac7168f2e86e167afc6250465
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-09 23:53:38 +0000

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,14 +8,14 @@
 
 void print_line(int n)
 {
-	int c;
-	if (n > 0)
-	{
-		for (c = 0; c <= n; c++)
-		{
-			_putchar('_');
-		}
+int c;
+if (n > 0)
+{
+for (c = 0; c <= n; c++)
+{
+_putchar('_');
+}
 
-	}
-	_putchar('\n');
+}
+_putchar('\n');
 }
