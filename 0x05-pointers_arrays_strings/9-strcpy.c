8719ee3e2efb8024248cadc0a8bfa2ebc42abee9
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-13 15:12:49 -0700

    Do you copy?

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string
+ * @dest: buffer
+ * @src: string
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (dest);
+}
