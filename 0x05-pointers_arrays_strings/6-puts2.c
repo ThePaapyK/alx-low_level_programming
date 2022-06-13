ea818de38cfaa1532b1a53e18139eb6e7afd3cca
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-13 18:48:43 +0000

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts - prints a string followed by a new line
+ * puts2 - prints a string followed by a new line
  * @str: string to be printed
  * Return: Nothing
  */
