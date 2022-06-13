b159d0b6f0dce7a054890f7c3234f0ffdbcf9752
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-13 23:12:45 +0000

    a to i

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,11 +9,29 @@
 
 int _atoi(char *s)
 {
-	int i, a;
+	int i, a, c;
 
+	
+	a = 0;
+	c = -1;
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] > 0 || s[i] <= 0)
+		if (s[i] == '-')
+			c *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
 		{
-			a = s[i];
-			
+			if (a < 0)
+				a = (a * 10) - (s[i] - '0');
+			else
+				a = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
+		}
+	}
+	if (c < 0)
+		a *= -1;
+
+	return (a);
+}
