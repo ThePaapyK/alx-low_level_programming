ba54e767ef62e5c9f5ea85c27a029636ab2d5ac2
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-13 18:01:15 +0000

    In reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,16 +8,13 @@
 
 void print_rev(char *s)
 {
-	char a[100];
-
-	a[0] = *s;
-
 	int i;
 
-	for (i--; i >= 0; i--)
+	for (i = 0; s[i] != '\0'; i++)
+		;
 
-	{
-		_putchar(a[i]);
-	}
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+
 	_putchar('\n');
 }
