3171fd65c78c151b4764ca78bda3c2e2ae8f78a9
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-13 10:16:04 -0700

    In reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "mnain.h"
+
+/**
+ * print_rev - prints a string in reverse folowed by a new line
+ * @s: string to be printed
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	char *a;
+
+	for (*a = '\0'; *a >= *s; a--;)
+
+	{
+		_putchar(*a);
+	}
+	_putchar('\n');
+}
