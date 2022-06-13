5ad9204a62ee120caaca2b7ac74d7ce5897c7867
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-13 13:54:04 -0700

    print n elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
+		_putchar(a[i]  + '0');
 
-		if (i < (n -1))
+		if (i < (n - 1))
 		{
 			_putchar(',');
 			_putchar(' ');
