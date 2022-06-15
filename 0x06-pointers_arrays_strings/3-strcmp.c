b30452bae24c924da5801d8c9b69e7e82d6801cf
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-15 14:00:22 -0700

    compare them

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,7 +12,12 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, j;
 
-	for (i = 0, j = 0; s1[i] != '\0', s2[j] != '\0'; i++, j++)
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		;
+	}
+
+	for (j = 0; s2[j] != '\0'; j++)
 	{
 		;
 	}
