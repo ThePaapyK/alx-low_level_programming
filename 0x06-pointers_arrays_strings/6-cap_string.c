8323888157008c0128ae92bba316226a600f8164
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-17 14:46:26 -0700

    Capitalize the words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words in a string
+ * @s: string
+ * Return: pointer to s
+ */
+
+char *cap_string(char *s);
+{
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i - 1] == 20, 21, 22, 28, 29, 44, 46, 59, 123, 125, 63, '\t', '\n')
+{
+if (s[i] > 96 && s[i] < 123)
+{
+s[i] = s[i] - 32;
+}
+}
+}
+return (s);
+}
