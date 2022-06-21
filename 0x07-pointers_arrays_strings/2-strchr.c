93b95e667c1d7757603e1ae9e8be89d5d0170e6d
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-21 13:35:40 -0700

    first occurrence

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Returns a pointer to the first occurrence of the character c in
+ * the string s, or NULL if the character is not found
+ * @s: string
+ * @c: character to be found
+ * Return: ponter to c, or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (&(s[i]));
+		}
+		break;
+	}
+
+	if (s[i] == '\0')
+		return (NULL);
+}
