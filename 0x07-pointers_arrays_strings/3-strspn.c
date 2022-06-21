2b95ac9454a11ae03ff8e84c4f39bc22a012a640
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-21 14:40:56 -0700

    find the common initial ones

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn - Returns the number of bytes in the initial segment of s
+ * which consists of only bytes from accept
+ * @s: memory area
+ * @accept: second memory area
+ * Return: number of bytes
+ */
+
+char *_strspn(char *s, char *accept)
+{
+	int i, j;
+
+	char a = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				a++;
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+			break;
+	}
+	return (a);
+}
