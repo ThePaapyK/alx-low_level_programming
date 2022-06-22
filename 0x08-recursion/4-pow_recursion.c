7c8633bd84e67f065026fa82942a7fc19e564bb5
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-22 09:27:56 -0700

    exponent

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,10 +13,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else if (y = 0)
+	else if (y == 0)
 	{
 		return (1);
 	}
 
-	return (x * pow_recursion(x, y));
+	return (x * _pow_recursion(x, y - 1));
 }
