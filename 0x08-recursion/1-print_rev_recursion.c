937db92cfac8e159b37f4ea92783c6f7fc44a04e
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-22 06:11:29 -0700

    in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to be printed
+ * Return: Nothing
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*(s - 1));
+		s--;
+	}
+	_print_rev_recursion(s);
+}
