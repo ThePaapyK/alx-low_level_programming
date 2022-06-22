0c57a1463a941eaaaeaef2bf035184244b5b0b2d
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-06-22 04:58:38 -0700

    print the string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,11 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
 
-	_putchar(s[i]);
-	i++;
 
-	if (s[i] != '\0')
-		_puts_recursion(s[i]);
+	_putchar(*s);
+	s++;
+
+	if (*s != '\0')
+		_puts_recursion(s);
 }
