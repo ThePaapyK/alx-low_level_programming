a5a377122791c7164ce0b79b110e7222858a4198
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-23 23:01:50 +0000

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+* is_palindrome - checks if a string is a palindrome
+* @s: string
+* Return: 1 if palindrome, 0 if not.
+*/
+
+int is_palindrome(char *s)
+{
+	int c = _strlen_recursion(s);
+	
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	if (s[0] == s[c -1])
+	{
+		return (1);
+	}
+	c--;
+	is_palindrome(s + 1);
+}
+
+/**
+ * _strlen_recursion - return length of a string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int c;
+
+	c = 0;
+	if (*s == '\0')
+		return (0);
+
+	c = 1 +  _strlen_recursion(s + 1);
+
+	return (c);
+}
