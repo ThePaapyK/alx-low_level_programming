9fddc274f4d321c4da5e784bd62c9733ce928746
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-24 15:27:14 +0000

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _sqrt(int n, int i);
+
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number
@@ -8,17 +10,29 @@
 
 int _sqrt_recursion(int n)
 {
-	int i = 1;
+	_sqrt(n, 1);
+}
 
+
+/**
+* _sqrt - finds square root
+* @n: number whose square root is to be found
+* @i: incrementer
+* Return: square root of `n`
+*/
+
+int _sqrt(int n, int i)
+{
 	if (i * i == n)
 	{
 		return (i);
 	}
 	else if (i * i < n)
 	{
-		i++;
-
+		return (_sqrt(n, i));
 	}
-	return (_sqrt_recursion(n));
-
+	else
+	{
+		return (-1);
+	}
 }
