da9580136bc0bd24a11502f310f59582aa724374
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-24 22:26:58 +0000

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* main - main block
+* @argc: number of command line arguments
+* @argv: array of pointers to strings
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	_putchar(*argv);
+	
+	return (0);
+}
