f42eb28f5f5c5a2a505bf812dc564e331215d6ce
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-24 02:41:02 +0000

    Update 101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -38,10 +38,10 @@ int wildcmp(char *s1, char *s2)
 	else if (*s2 == '*')
 	{
 		if (*(s2 + 1) == '*')
-			return wildcmp(s1, ++s2);
+			return (wildcmp(s1, ++s2));
 		else
 		{
-			return wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2);
+			return (wildcmp(s1 + findsrc(s1, *(s2 + 1), 0, 0), s2));
 		}
 	}
 
@@ -50,7 +50,7 @@ int wildcmp(char *s1, char *s2)
 }
 
 /**
- * finsrc - compare two strings with "wildcard expansion" capabilities
+ * findsrc - compare two strings with "wildcard expansion" capabilities
  * @s: string 1
  * @c: character
  * @i: counter
