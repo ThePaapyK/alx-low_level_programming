a425db7c7b556a98167027a67bbe2e6ed41e8c51
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-24 22:47:56 +0000

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - main block
+* @argc: number of command line arguments
+* @argv: array of command line arguments
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	
+	_putchar(argc);
+	
+	return (0);
+}
