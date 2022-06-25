2b511bff90e2c93187426da304e107088d0ae2c3
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-25 01:07:24 +0000

    Create 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - adds numbers
+* @argc: argument counter
+* @argv: argument vector
+* Return: Sum of arguments
+*/
+
+int main(int argc, char **argv)
+{
+	int i;
