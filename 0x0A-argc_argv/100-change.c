d81df351c49178f18c894fb73e01105369d49e32
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-25 23:44:58 +0000

    Create 100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - main block
+* @argc: argument count
+* @argv: argument vector
+* Return: (0)
+*/
+
+int main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		if (argv[1] > 0)
+		{
+			int cents = 100 * atoi(argv[1]);
+
+			printf("%d\n", cents);
+		}
+		else
+		{
+			printf("0\n");
+		}
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+	return (0);
+}
+		
