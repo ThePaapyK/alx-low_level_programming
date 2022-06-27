d77e05f8e151ebbbe69b99395745e637ac14b439
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-27 12:38:53 +0000

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,17 +15,17 @@ char *_strdup(char *str)
 	int i, j;
 	char *s;
 
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
 	for (i = 0; str[i] != '\0'; i++)
 		;
 
 	s = malloc(i * sizeof(char) + 1);
 
 	if (s == NULL)
-	{
-		return (NULL);
-	}
-
-	if (str == NULL)
 	{
 		return (NULL);
 	}
