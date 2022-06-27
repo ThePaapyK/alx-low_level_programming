666413199b2a43d892c41bfcaf3c467d525e66a4
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-27 23:15:17 +0000

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,27 +14,24 @@
 char *argstostr(int ac, char **av)
 {
 	int i, j;
-	char **s;
+	char *s;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	s = malloc(ac * sizeof(*av));
+	s = malloc(ac * sizeof(*av) + 1);
 	
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < ac; i++)
+	for (i = 1; i < ac; i++)
 	{
-		s[i] = malloc(sizeof(av) + 1);
+		s[i] = malloc(sizeof(av[i]));
 		
-		for (j = 0; av[i][j] != '\0'; j++)
-		{
-			s[i][j] = av[i][j];
-		}
-		s[i][j] = '\n';
+		s[i] = av[i];
+
 	}
-	return (*s);
+	return (s);
 	free(*s);
 	free(s);
 }
