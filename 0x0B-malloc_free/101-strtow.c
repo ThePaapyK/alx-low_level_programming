ba6aca281cb467b0d49d62574ce5c55fd0cd4a16
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-28 23:06:48 +0000

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,65 +1,84 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
-* strtow -  splits a string into words
-* Description: Each element of this array should contain
-* a single word, null-terminated.
-* The last element of the returned array should be NULL.
-* Words are separated by spaces.
-* @str: string to be split
-* Return: a pointer to an array of strings (words)
-* or NULL if str == NULL or str == "".
-*/
+ * _wcount - counts number of words
+ * @sw: string
+ *
+ * Return: int
+ */
+int _wcount(char *sw)
+{
+	int l, wc;
 
+	l = 0, wc = 0;
+	if (*(sw + l) == ' ')
+		l++;
+	while (*(sw + l))
+	{
+		if (*(sw + l) == ' ' && *(sw + l - 1) != ' ')
+			wc++;
+		if (*(sw + l) != ' '  && *(sw + l + 1) == 0)
+			wc++;
+		l++;
+	}
+	return (wc);
+}
+/**
+ * _trspace - Moves adress to remove trailig whitespaces
+ * @st: string
+ *
+ * Return: Pointer
+ */
+char *_trspace(char *st)
+{
+	while (*st == ' ')
+		st++;
+	return (st);
+}
+/**
+ * strtow - splits a string into words
+ * @str: string
+ *
+ * Return: Double Pointer
+ */
 char **strtow(char *str)
 {
-	int i, j, l, sp, k;
-	char **tow;
+	char **s, *ts;
+	int l, l2, wc, i, j, fr, k;
 
-	if (str == NULL || *str == ' ')
-		return (NULL);
-
-	sp = 0;
-
-	for (i = 0; str[i] != '\0'; i++)
+	if (str == NULL || *str == 0)
+		return (0);
+	fr = 0;
+	wc = _wcount(str);
+	if (wc == 0)
+		return (0);
+	s = malloc((wc + 1) * sizeof(char *));
+	if (s == 0)
+		return (0);
+	ts = _trspace(str);
+	for (i = 0; i < wc; i++)
 	{
-		if(str[i] == ' ')
-			sp++;
+		l = 0;
+		while (*(ts + l) != ' ' && *(ts + l) != 0)
+			l++;
+		s[i] = malloc((l + 1) * sizeof(char));
+		if (s[i] == 0)
+		{
+			fr = 1;
+			break;
+		}
+		for (j = 0, l2 = 0; l2 < l; l2++, j++)
+			s[i][j] = *(ts + l2);
+		s[i][j] = '\0';
+		ts = _trspace(ts + l);
 	}
-	sp++;
-
-	tow = malloc(i * sizeof(char) + 1);
-
-	if (tow == NULL)
-		return NULL;
-
-	j = 0;
-
-	for (k = 0; k < sp; k++)
+	s[i] = NULL;
+	if (fr == 1)
 	{
-
-		for (l = 0; str[l + j + 1] != ' '; l++)
-			;
-
-		tow[k] = malloc(sizeof(char) * l + 1);
-
-		if (tow[k] == NULL)
-		{
-			for (--k; k >= 0; k--)
-				free(tow[k]);
-			free(tow);
-			return (NULL);
-		}
-
-		for (j = 0; str[j] != ' '; j++)
-		{
-			tow[k][j] = str[j];
-		}
-		tow[k][j] = '\0';
+		for (k = 0; k <= i; k++)
+			free(s[k]);
+		free(s);
 	}
-	tow[k][j] = '0';
-
-	return (tow);
-	free(tow);
+	return (s);
 }
