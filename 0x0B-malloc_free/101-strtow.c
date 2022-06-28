8c0db9b5937d1570163e91791a87fbb426213485
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-06-28 21:47:20 +0000

    make an array out of a string

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,66 @@
+#include "main.h"
+#include <stdlib>
+
+/**
+* strtow -  splits a string into words
+* Description: Each element of this array should contain
+* a single word, null-terminated.
+* The last element of the returned array should be NULL.
+* Words are separated by spaces.
+* @str: string to be split
+* Return: a pointer to an array of strings (words)
+* or NULL if str == NULL or str == "".
+*/
+
+char **strtow(char *str)
+{
+	int i, j, sp, k;
+	char **tow;
+
+	if (str == NULL || str == "")
+		return (NULL);
+
+	sp = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if(str[i] == " ")
+			sp++;
+	}
+	sp++;
+
+	tow = malloc(i * sizeof(char) + 1);
+
+	if (tow == NULL)
+		return NULL;
+
+	j = 0;
+
+	for (k = 0; k < sp; k++)
+	{
+
+		for (l = 0; str[l + j + 1] != " "; l++)
+			;
+
+		tow[k] = malloc(sizeof(char) * l + 1);
+
+		if (tow[k] == NULL)
+		{
+			for (--k; k >= 0; k--)
+				free(tow[k]);
+			free(tow);
+			return (NULL);
+		}
+
+		for (j = 0; str[j] != " ")
+		{
+			tow[k][j] = str[j];
+		}
+		tow[k][j] = '\0';
+	}
+	tow[k][j] = NULL;
+	
+	return (tow);
+	free(tow);
+}
+	
