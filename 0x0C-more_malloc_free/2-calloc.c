bcedef672007b9907f9a069b813acd69e938937c
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-01 01:08:01 +0000

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *s;
 	int n;
 
-	if (nmemb = 0 || size == 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	s = malloc(nmemb * size);
