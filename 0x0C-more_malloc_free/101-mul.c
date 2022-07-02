69d6754b8f33502913f31d9d30b31d38d8b90762
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-02 22:59:48 +0000

    Create 101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* allo - allocates memory for a positive integer
+* @n: number
+* 
+* Return a pointer to the number
+*/
+
+unsigned long int *allo(unsigned long int *n)
+{
+	n = malloc(sizeof(*n);
+		   return (n);
+		   }
