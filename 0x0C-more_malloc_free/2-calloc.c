74d68b5057e01574acea88598f5b4121c5a3a44e
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-02 13:48:33 +0000

    Update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,6 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * _memset - fills the first n bytes of the memory area pointed to by s with
+ * the constant byte b
+ * @s: memory area
+ * @b: constant byte
+ * @n: number of bytes to be filled
+ * Return: the pointer to s
+ */
+
 char *_memset(char *s, char b, unsigned int n)
 {
 	char *beat;
