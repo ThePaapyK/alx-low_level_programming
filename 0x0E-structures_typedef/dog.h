217101d21dd65d2a72323d215278987e0a24087c
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-04 23:19:27 +0000

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,13 +14,4 @@ struct dog
 	float age;
 	char *owner;
 };
-#endif
-
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
-#endif 
+#endif /* DOG_H */
