53330292414336321758c7f298eb25c511a41abe
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-04 23:32:47 +0000

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,10 +12,23 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
-
-		printf("Name: %s\n", d->name);
+		if (d->name == 0)
+		{
+			printf("Name: %s\n", "(nil)");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d->owner == 0)
+		{
+			printf("Owner: %s\n", "(nil)");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
 	}
 }
 		
