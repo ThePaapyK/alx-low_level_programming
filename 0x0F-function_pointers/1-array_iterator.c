387c406d295e722d74c06a117ea2a8b9a790c586
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-06 17:29:15 +0000

    Update 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	int c = (int)size / sizeof(array[0]);
+	int c = (int)size / sizeof(int);
 
-	for (i = 0; i <= c; i++)
+	for (i = 0; i < c; i++)
 	{
 		action(array[i]);
 	}
