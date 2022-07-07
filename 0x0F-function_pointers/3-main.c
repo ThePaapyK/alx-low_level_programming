b47b7dfca7f6af0509ba30fc917a6ba4da29b8ed
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-07 00:47:07 +0000

    Create 3-main.c

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - performs simple operations.
+* @argc: argument counter
+* @argv: argument vector
+* Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2] != '+' || argv[2] != '-' || argv[2] != '*' || argv[2] != '/' || argv[2] != '%')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2] == '/' || argv[2] == '%') && (argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	int oper = (*get_op_func(argv[2]))(argv[1], argv[3]);
+
+	printf("%d\n", oper);
+
+	return (0);
+}
