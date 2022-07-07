493b98e4244c9f2fcc42e0d58b4039383ce8c58a
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-07 01:19:54 +0000

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,12 +11,14 @@
 
 int main(int argc, char **argv)
 {
+	int oper;
+	
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2] != '+' || argv[2] != "-" || argv[2] != "*" || argv[2] != "/" || argv[2] != "%")
+	if (argv[2] != "+" || argv[2] != "-" || argv[2] != "*" || argv[2] != "/" || argv[2] != "%")
 	{
 		printf("Error\n");
 		exit(99);
@@ -27,7 +29,7 @@ int main(int argc, char **argv)
 		exit(100);
 	}
 
-	int oper = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	oper = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 
 	printf("%d\n", oper);
 
