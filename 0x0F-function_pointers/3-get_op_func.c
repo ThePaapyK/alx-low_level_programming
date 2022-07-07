d9a2c268c4d2c0d6f22dcfc6e77bff51e14c07a0
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-07 00:26:48 +0000

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* get_op_func - gets the right operator to work
+* @get_op_func: pointer to operator function
+* @s: pointer specific operator
+* @a: first argument
+* @b: second argument
+* Return:  pointer to the function that corresponds
+* to the operator given as a parameter
+*/
+
+int (*get_op_func(char *s))(int a, int b)
+{
+	int i = 0;
+
+	void (*fun_ptr_arr[])(int, int) = {op_add; op_sub, op_mul, op_div, op_mod};
+	char array[] = {'+', '-', '*', '/', '%'};
+	
+	while (i < 5 && )
+	{
+		if (s == array[i])
+		{
+			return ((*fun_ptr_arr[i])(a , b)); 
+
+		}
+	}
+	return (NULL);
+}
