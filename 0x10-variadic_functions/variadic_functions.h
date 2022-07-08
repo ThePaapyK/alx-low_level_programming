b23073b37f6b793c2f14dc0ab1d0665f3e524599
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-08 12:17:17 +0000

    Create variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,7 @@
+#ifndef VAR_FUN
+#define VAR_FUN
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#endif /* VAR_FUN */
