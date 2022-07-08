788bb88d8607a30fc4de0f28677cb9838545936d
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-08 21:42:07 +0000

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,14 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void printstr(va_list ap);
+void printint(va_list ap);
+void printchar(va_list ap);
+void printfloat(va_list ap);
+
+typedef struct doer
+{
+	char *daty;
+	void (*f)();
+} doer;
 #endif /* VAR_FUN */
