9bdc5724b1ead3f4f165ff96c187ffeaef851bcc
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-08 13:49:09 +0000

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,3 +21,27 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 	_putchar('\n');
 }
+
+/**
+* print n - prints integers
+* @n: integer to be printed
+*/
+
+void printn(long n)
+{
+	/** If number is smaller than 0, put a - sign
+	* and change number to positive
+	*/
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	/* Remove the last digit and recur*/
+	if (n / 10)
+		printn(n / 10);
+
+	/* Print the last digit */
+	_putchar(n % 10 + '0');
+}
