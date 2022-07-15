d933f794a139b170951404796a90d9e1fa1f2ed7
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-15 14:32:07 -0700

    Yo!

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
-* add_node - adds a new node at the end of a list_t list.
+* add_node_end - adds a new node at the end of a list_t list.
 * @head: list
 * @str: node to be added
 * Return: pointer to newly added node
@@ -38,14 +38,12 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	sire->len = i;
 	sire->next = NULL;
-<<<<<<< HEAD
-	c_node->next = sire;
-=======
+
 
 	if (c_node)
 		c_node->next = sire;
 	else
 		*head = sire;
->>>>>>> 425b01d36ec607269f9b1a9baeff6532ea15a780
+
 	return (sire);
 }
