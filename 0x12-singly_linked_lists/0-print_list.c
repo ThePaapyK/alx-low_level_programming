c0cf1d7d72d03aaa5a3f3a06b3349194afea7d56
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-15 16:48:09 +0000

    Create 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+* print_list - prints all the elements of a list_t list
+* @h: list to be printed
+* Return: Number of nodes
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i;
+	
+	for (i = 0; h->next != NULL; i++)
+	{
+		printf("%s", h->str);
+		h = h->next;
+	}
+	return (i);
+}	
