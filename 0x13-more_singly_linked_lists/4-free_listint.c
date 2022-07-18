a2340fcee62b852dfb2371d01ab12c1c54b9d03e
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-18 22:08:35 +0000

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -9,10 +9,13 @@
 
 void free_listint(listint_t *head)
 {
-	if(head->next)
+	if (head)
 	{
+		while (head->next)
+		{
+			free(head);
+			head = head->next;
+		}
 		free(head);
-		head = head->next;
 	}
-	free(head);
 }
