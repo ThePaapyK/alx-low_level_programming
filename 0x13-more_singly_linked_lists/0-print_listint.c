da45bcffee4b3953307cd7f5f99028e2dbae5e20
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-18 22:29:40 +0000

    God of Elijah

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,10 +9,10 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t i;
-	
-	for (i = 0; h->next; i++)
+
+	for (i = 0; h != NULL; i++)
 	{
-		printf("%d\n", h.n);
+		printf("%d\n", h->n);
 		h = h->next;
 	}
 	return (i);
