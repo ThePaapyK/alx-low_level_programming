c668db7b2bcb001e15d0954745c7908976423e8f
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-19 21:34:54 +0000

    Hope this one works

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,10 +10,12 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	int i;
-	
+	unsigned int i;
+
 	for (i = 0; i < index; i++)
 	{
+		if (i == index)
+			return (head);
 		head = head->next;
 	}
 	if (head == NULL)
