6f225563d81edaa4e492304ea1cfa37c97daf184
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-20 14:36:00 +0000

    Jesus

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	unsigned int i = 0;
 
+	if (c_node == NULL)
+		return (NULL);
+	
 	if (idx != 0)
 	{
 		while (c_node && i < idx - 1)
