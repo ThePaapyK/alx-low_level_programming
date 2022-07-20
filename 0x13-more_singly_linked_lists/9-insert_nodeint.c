502f86aa111aba5a19fb0e2e2bf6a043abc87faf
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-20 14:44:12 +0000

    Jesus

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,9 +19,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	unsigned int i = 0;
 
-	if (c_node == NULL)
-		return (NULL);
-
 	if (idx != 0)
 	{
 		while (c_node && i < idx - 1)
@@ -32,9 +29,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		}
 	}
 
+	if (c_node == NULL && idx != 0)
+		return (NULL);
 
 	sire = malloc(sizeof(listint_t));
-	if (!sire || !c_node || (!sire && !c_node))
+	if (!sire)
 	{
 		free(sire);
 		return (NULL);
