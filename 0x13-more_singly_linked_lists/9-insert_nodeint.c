cc0955d94c4d091286702e650fe1a0864158d011
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-20 14:33:30 +0000

    Jesus

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 
 	sire = malloc(sizeof(listint_t));
-	if (!sire)
+	if (!sire || !c_node || (!sire && !c_node))
 	{
 		free(sire);
 		return (NULL);
