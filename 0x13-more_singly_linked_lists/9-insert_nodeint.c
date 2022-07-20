8fd099f392d20c6ffab97c248fcf787a9bd64673
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-20 00:10:53 +0000

    GOd of mercy

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -40,6 +40,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		temp = c_node->next;
 		sire->next = temp;
 		c_node->next = sire;
+
+		if (!sire)
+			return (NULL);
 	}
 	else
 	{
