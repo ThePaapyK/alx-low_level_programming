e7825e83c729b32730ac261b4e4f69d4bc2457d2
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-20 14:07:44 +0000

    Please work

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *c_node = *head;
 
 	unsigned int i = 0;
+
 	if (idx != 0)
 	{
 		while (c_node && i < idx - 1)
