b298e04b5917ee93aa586255b37a6c27559df91d
ThePaapyK <99420875+ThePaapyK@users.noreply.github.com>
2022-07-20 18:02:37 +0000

    Update 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,27 +11,17 @@ listint_t *reverse_listint(listint_t **head)
 	listint_t *c_node;
 	listint_t *h;
 
-	if (*head == NULL)
-		return (NULL);
+	c_node = NULL;
+	h = NULL;
 
-	c_node = *head;
-	*head = c_node->next;
-	h = (*head)->next;
-	c_node->next = NULL;
-	if (*head == NULL)
+	while (*head != NULL)
 	{
-		*head = c_node;
-		return (c_node);
-	}
-
-	while (h != NULL)
-	{
+		h = (*head)->next;
 		(*head)->next = c_node;
 		c_node = *head;
 		*head = h;
-		h = (*head)->next;
 	}
 
-	(*head)->next = c_node;
+	*head = c_node;
 	return (*head);
 }
