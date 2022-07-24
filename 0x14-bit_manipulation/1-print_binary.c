766bcbfd3d981a4df047739ea811ba74442c847e
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-24 02:33:20 +0000

    Popular

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,35 +1,15 @@
 #include "main.h"
 
-/**
-* print_binary - prints the binary representation of a number.
-* n: number in decimal
-*/
-
 void print_binary(unsigned long int n)
 {
-	unsigned int i, a;
-
-	for (i = 0; _pow(2, i) > n; i++)
-			;
-	i--;
-	a = _pow(2, i);
-	_putchar('1');
-
-	while (n > 0)
+	if (n >> 0)
 	{
-		n = n - a;
-		for (i--; n; i--)
-		{
-			if (n - _pow(2, i) < 0)
-			{
-				_putchar('0');
-			}
-			else
-			{
-				_putchar('1');
-				a = _pow(2, i);
-				break;
-			}
-		}
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
+	else
+	{
+		_putchar('0');
 	}
 }
