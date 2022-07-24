a7c90e830591c8704f6ed438afdeb57bcceb8b6f
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-07-24 02:36:55 +0000

    Popular

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: unsigned long integer
+ * Return: Nothing
+ */
 
 void print_binary(unsigned long int n)
 {
