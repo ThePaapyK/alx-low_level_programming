cbbb7a3794cf197dbc1231c6bbea2b7bd82f9010
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-08-10 19:09:24 +0000

    again oogit status!

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * read_textfile - reads a textfile and prints
  * it to the POSIX standard output
  * @filename: path to the file to the textfile
- * @letters number of letters to be read and printed out
+ * @letters: number of letters to be read and printed out
  *
  * Return: The actual number of letters it could read and print.
  * if the file can not be opened or read, return 0.
@@ -16,7 +16,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t rd, wr;
-	char * buff;
+	char *buff;
 
 	int fd = open(filename, O_RDWR);
 
@@ -26,7 +26,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 
-	buff = malloc(sizeof(char) * letters);
+	buff = malloc(sizeof(char) * (letters));
 	if (!buff)
 		return (0);
 
@@ -42,5 +42,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	close(fd);
 	free(buff);
-	return(wr);
+	return (wr);
 }
