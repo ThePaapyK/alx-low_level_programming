101c6aaee43f27549aab8e76b0b2fe87734ca910
Paa Kojo Annan <paaqwadwo419@gmail.com>
2022-11-13 01:04:18 +0000

    Hey there!

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -20,9 +20,9 @@ def island_perimeter(grid):
                     if y < len(grid) - 1:
                         if grid[y + 1][x] != 0:
                             perimeter -= 1
-                    if x > 0:
-                        if grid[y][x - 1] != 0:
-                            perimeter -= 1
+                if x > 0:
+                    if grid[y][x - 1] != 0:
+                        perimeter -= 1
                     if x < len(grid[0]) - 1:
                         if grid[y][x + 1] != 0:
                             perimeter -= 1
