de7862e767a18cffbef0cfeaa50e6eb9422d5391
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-09 00:13:42 +0000

    correction

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -24,7 +24,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->n = n;
 	if (idx == 0)
 	{
-		add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 	}
 	idx--;
 	while (node && i != idx)
