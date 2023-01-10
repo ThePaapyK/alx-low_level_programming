86b261b57a9ef34758fb5aca03cca4b5db8d6317
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-10 19:04:40 +0000

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main (void) {
-
+int main(void)
+{
 	srand((unsigned int)(time(NULL)));
-	int limit = 2772, random;
+	int random;
+
+	int limit = 2772;
 
 	char char1[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/,.-+=~`<>:";
+
 	while (limit > 126)
 	{
-		random = char1[rand() % (sizeof char1 - 1)];
+		random = char1[rand() % (sizeof(char1 - 1))];
 		limit -= random;
 		printf("%c", random);
 	}
 	if (limit > 0)
 		printf("%c", limit);
-	return 0;
+	return (0);
 }
