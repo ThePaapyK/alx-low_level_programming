70ce003d935e7eef1fa9dc342686c91866bcccfa
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-13 15:37:37 +0000

    insert into hash table

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -36,5 +36,5 @@ typedef struct hash_table_s
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-hash_node_t *create_node(char *key, char *value);
+hash_node_t *create_node(const char *key, const char *value);
 #endif /*HASH_TABLE_H*/
