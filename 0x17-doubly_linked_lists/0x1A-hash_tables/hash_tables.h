503181ee42655917d47e1bda1701184844ad53d9
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-13 12:08:06 +0000

    include guard: header file

--- 0x17-doubly_linked_lists/0x1A-hash_tables/hash_tables.h
+++ 0x17-doubly_linked_lists/0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,8 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -26,3 +31,6 @@ typedef struct hash_table_s
 	unsigned long int size;
 	hash_node_t **array;
 } hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+#endif /*HASH_TABLE_H*/
