ac0ba5272ca856a9f91a1aa985330be67eb4d7c6
Paa Kojo Annan <paaqwadwo419@gmail.com>
2023-01-13 23:57:35 +0000

    free the hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete -  deletes a hash table.
+ * @ht: hash table to be deleted
+ * Return: Nothing
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *cur_node;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i])
+		{
+			cur_node = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = cur_node;
+		}
+	}
+	
+	free(ht->array);
+	free(ht);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,4 +40,7 @@ hash_node_t *create_node(const char *key, const char *value);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void free_item(hash_node_t *item);
+void free_table(hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 #endif /*HASH_TABLE_H*/
